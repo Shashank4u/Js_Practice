c4d1aef67fd480718c525d0b662d86666d38caa4
Shashank <shashankgurjar4u@gmail.com>
2024-01-09 15:23:56 +0530

    Practice Project

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("helloworl!");
+// function cVowels(str){
+//     let count=0;
+//     for(let char of str){
+//         if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+//             count++;
+//         }
+
+//     }
+//     console.log(count);
+// }
+//arrow function
+
+// const countVow=(str)=>{
+//     let count=0;
+//     for(let char of str){
+//         if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+//for Each loop for array
+
+let arr=[1,2,3,4,5];
+arr.forEach((element) => {
+    console.log(element*element);
+});
+
+
+
+
+
+
+
+
+
+
+//Artimetic operators
+//let a=5;
+//let b=2;
+// let num =prompt("enter the number");
+// if(num>=80&&num<=100){
+//     console.log("A");
+// }else if(num>=70&&num<=89){
+//     console.log("B");
+// }
+// let str=prompt("inter user name");
+// let str2="@";
+
+// console.log(str2.concat(str)+str.length);
+
+//console.log("a^b=",a**b);give power
+//Assigment operators 
+//a+=4;//a=a+4
+//console.log(a);
+//triple equalsto is used for stricly cheack that type cheack  ====
